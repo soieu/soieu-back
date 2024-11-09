3bb5d2c6b099c9d9e198984d995b1ba11e2d10e8
soieu <102034740+soieu@users.noreply.github.com>
2024-11-09 22:39:34 +0900

    Initial commit

---
+++ src/main/kotlin/com/soieu/soieuback/SoieuBackApplication.kt
@@ -0,0 +1,11 @@
+package com.soieu.soieuback
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class SoieuBackApplication
+
+fun main(args: Array<String>) {
+    runApplication<SoieuBackApplication>(*args)
+}
